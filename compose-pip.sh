9fc207a724dcb7199b5b7841407a9296f850eca6
Suresh Thumma <srthumma@yahoo.com>
2016-07-14 11:27:39 +0530

    Update compose-pip.sh

--- compose-pip.sh
+++ compose-pip.sh
@@ -1,7 +1,11 @@
 yum update -y 
 yum install -y epel-release
+yum update -y 
 yum install -y python-pip
 pip install docker-compose
+pip install --upgrade pip
+pip install docker-compose --upgrade
 yum upgrade python*
+
 
 
